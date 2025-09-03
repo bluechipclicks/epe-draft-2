d6a59bc09e67909f837d1860b4e6314eac557772
bluechipclicks <bluechipclickssocial3@gmail.com>
2025-09-03 14:12:33 +0530

    about text changed

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -4,10 +4,17 @@ import Image from "next/image";
 const AboutUs = () => {
   return (
     <section className="about-sections bg-cover bg-no-repeat bg-center py-16 relative">
-      <div className="container mx-auto px-4">
+      <div className=" mx-auto px-4">
         <div className="about-wrapper relative">
           <div className="about-title mb-8">
-            <h2 className="main-heading text-white uppercase leading-none text-[6vw] text-center mx-auto w-full mb-8">
+            <h2
+              className="
+              text-[6vw]  mx-auto w-full mb-8
+                           text-center
+                           text-5xl md:text-[6vw] font-extrabold text-transparent  about-hover-fill-text relative cursor-pointer whitespace-nowrap
+                           "
+              data-text={"About Energy Process Equipments"}
+            >
               About Energy Process Equipments
             </h2>
           </div>
